9edc268a07e0d9e3f091b33b96164eb128499c92
Lucía Domínguez Rodrigo <117301166+LuciaDominguezRodrigo@users.noreply.github.com>
2025-02-01 15:44:11 +0100

    sadad
    qweqwe

--- snakePy.py
+++ snakePy.py
@@ -8,7 +8,7 @@ from tkinter import messagebox
 
 
 class cube(object):
-    rows = 20
+    rows = 20dfdfdfdfdter34wtdtf
     w = 500
 
     def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
@@ -17,10 +17,10 @@ class cube(object):
         self.dirny = 0
         self.color = color
 
-    def move(self, dirnx, dirny):
+    def move(self, dirdfyertyertrnx, dirny):
         self.dirnx = dirnx
         self.dirny = dirny
-        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+        self.pos = (self.pos[0] + erterteeself.dirnx, self.pos[1] + self.dirny)
 
     def draw(self, surface, eyes=False):
         dis = self.w // self.rows
@@ -29,11 +29,11 @@ class cube(object):
 
         pygame.draw.rect(surface, self.color, (i * dis + 1, j * dis + 1, dis - 2, dis - 2))
         if eyes:
-            centre = dis // 2
+            centre = dis // 2rtertert
             radius = 3
             circleMiddle = (i * dis + centre - radius, j * dis + 8)
             circleMiddle2 = (i * dis + dis - radius * 2, j * dis + 8)
-            pygame.draw.circle(surface, (0, 0, 0), circleMiddle, radius)
+            pygame.draw.circle(serterteturface, (0, 0, 0), ciertertertrcleMiddle, radius)
             pygame.draw.circle(surface, (0, 0, 0), circleMiddle2, radius)
 
 
